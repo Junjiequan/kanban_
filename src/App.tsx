9b9feba901021263cc7b3d144e831f1011796177
mmmG <a331998513@gmail.com>
2022-06-13 02:52:09 +0200

    remove vite templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [colorTheme, setColorTheme] = useState('light');
+  const handleColorTheme = () => {
+    return colorTheme === 'light' ? setColorTheme('dark') : setColorTheme('light');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App' data-theme={colorTheme}>
+      <button onClick={handleColorTheme}> Click to change color</button>
+      <div>Hello</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

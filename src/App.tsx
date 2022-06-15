8680c7de83ebf035901b1a3b1960dd10128270ec
mmmG <a331998513@gmail.com>
2022-06-15 04:04:37 +0200

    tailwind properties are discusting - removed

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,12 @@ const App = () => {
     return colorTheme === 'light' ? setColorTheme('dark') : setColorTheme('light');
   };
   return (
-    <div className='App ' data-theme={colorTheme}>
-      <button
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
-        onClick={handleColorTheme}
-      >
+    <div className='App' data-theme={colorTheme}>
+      <button className='' onClick={handleColorTheme}>
         {'NAH,....NOT TODAY '}
         Click to change color
       </button>
-      <div className='text-blue-900'>Hello</div>
+      <div className=''>Hello</div>
     </div>
   );
 };

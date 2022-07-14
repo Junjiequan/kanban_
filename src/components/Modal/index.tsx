dd1baee491a2e95e1907bf2898afb177eb0c4281
mmmG <a331998513@gmail.com>
2022-07-14 20:48:14 +0200

    folder structure

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Modal = () => {
+  return <div>index</div>;
+};
+
+export default Modal;
---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SideBar = () => {
+  return <div>index</div>;
+};
+
+export default SideBar;

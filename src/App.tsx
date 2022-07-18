75205d8070fd0fb040d434dfbb63c9c3d82e225e
mmmG <a331998513@gmail.com>
2022-07-18 19:18:59 +0200

    remove console

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,6 @@ const App = () => {
     return colorTheme === 'light' ? setColorTheme('dark') : setColorTheme('light');
   };
 
-  const value = ['one', 'two', 'three'];
-
-  // const test = value.reduce((acc, v) => {
-  //   return Object.assign(acc, { [v]: v });
-  // }, {});
-
-  const newMap = new Map();
-  for (let i = 0; i < value.length; i++) {
-    newMap.set(`${i}:${value[i]}`, value[i]);
-  }
-
-  console.log(Object.fromEntries(newMap));
 
   return (
     <div className='App' data-theme={colorTheme}>

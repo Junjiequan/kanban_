66dc10803ae76ac80245d04338c886ee5e972512
mmmG <a331998513@gmail.com>
2022-07-24 05:25:22 +0200

    scss removed unnecessary import

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Content = (props: any) => {
   const { hideSideNav } = props;
   const onHide = hideSideNav ? 'Content__full' : '';
-  return <div className={`Content ${onHide}`}>Content</div>;
+  return (
+    <div className={`Content ${onHide}`}>
+      <div className='Content__column'>asdsadsa</div>
+      <div className='Content__column'>asdsadsa</div>
+      <div className='Content__column'>asdsadsa</div>
+    </div>
+  );
 };
 
 export default Content;
